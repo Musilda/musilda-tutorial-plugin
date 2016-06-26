4f1526a199619abd390895f1e776173fd1dafc1f
Musilek Vladislav <musilekv@seznam.cz>
2016-06-26 17:30:46 +0200

    Create musilda-tutorial.php

---
+++ musilda-tutorial.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * @package   Musilda tutorial
+ * @author    Vladislav Musílek
+ * @license   GPL-2.0+
+ * @link      Musilda.cz
+ * @copyright 2016 Musilda.cz                             
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Musilda Tutoriál
+ * Plugin URI:        http://musilda.cz/jak-vytvorit-jednoduchy-plugin-pro-wordpress/
+ * Description:       Jednoduchý plugin pro WordPress
+ * Version:           1.0
+ * Author:            Vladislav Musílek
+ * Author URI:        http://musilda.cz
+ * Text Domain:       musilda-tutorial
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
